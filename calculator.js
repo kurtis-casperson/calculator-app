01bf00b0936aae79370f109827c64fcaa6217fc4
Kurtis <kurtis@edge.app>
2023-03-26 20:11:56 -0700

    Add styles to calculator

---
+++ calculator.js
@@ -0,0 +1,6 @@
+// event listeners for each button
+// when a number button is clicked is it added to an empty variable
+// when a operator is clicked the text box becomes empty and the next number can be input
+// clicking multiple operators.. the last operator clicked is the one used.
+// that will run when the = button is clicked
+// the clear button will remove the numbers from the variable
