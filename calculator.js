38de733868c4ce2e6d3ababe2712de60739447d4
Kurtis <kurtis@edge.app>
2023-04-03 11:30:02 -0700

    Add equation functionality for single digit operations

--- calculator.js
+++ calculator.js
@@ -1,15 +1,10 @@
-// when a number button is clicked is it added to an empty variable
-
-// when a operator is clicked the text box becomes empty and the next number can be input
-// clicking multiple operators.. the last operator clicked is the one used.
-// that will run when the = button is clicked
-// the clear button will remove the numbers from the variable
+// do 11 + 2 next
 
 const buttonEvents = document.querySelectorAll('[data-button]')
 const twobuttons = document.querySelectorAll('row-two')
 const buttons = document.getElementsByClassName('number-btn')
 const operators = document.getElementsByClassName('operator-btn')
-const equalButton = document.getElementById('equal-btn')
+
 const display = document.getElementById('numberDisplay')
 let equationArray = []
 
@@ -27,17 +22,17 @@ const buttonDisplayFunction = (buttonsArray) => {
 }
 
 // perform equations
+let first = ''
+let second = ''
+let firstOperator = equationArray[0]
+let secondOperator = equationArray[1]
+let operation = []
 
 const numberEvents = (buttonsArray) => {
   buttonsArray.forEach((button) => {
     button.addEventListener('click', () => {
       equationArray.push(button.value)
-      let value = button.value
-      console.log(value)
-      return value
-      // equationArray.push(button.value)
-
-      // add nubmer or decimal to the equationString
+      console.log(equationArray, 'equation-array')
     })
   })
 }
@@ -45,31 +40,42 @@ const numberEvents = (buttonsArray) => {
 const operatorEvents = (operatorArray) => {
   operatorArray.forEach((operator) => {
     operator.addEventListener('click', () => {
-      equationArray.push(operator.value)
-      let value = operator.value
-      console.log(value)
-      return value
-      // equationArray.push(operator.value)
-      console.log(operator.value)
-      // add operator to equationString
-      // if equationString begins [0] with an operator, then remove the operator from the string
+      if (operation != []) operation = []
+      operation.push(operator.value)
+      console.log(operation, 'operation')
     })
   })
 }
 
-const equationOptions = (equationArray) => {
-  equalButton.addEventListener('click', () => {
-    console.log(equationArray)
-  })
-}
+const equalButton = document.getElementById('equal-btn')
+
+equalButton.addEventListener('click', () => {
+  console.log(equationArray, 'array')
+  let equation = []
+
+  if (operation[0] === '+') {
+    display.textContent = Number(equationArray[0]) + Number(equationArray[1])
+  }
+  if (operation[0] === '-') {
+    display.textContent = Number(equationArray[0]) - Number(equationArray[1])
+  }
+  if (operation[0] === '*') {
+    display.textContent = Number(equationArray[0]) * Number(equationArray[1])
+  }
+  if (operation[0] === '/') {
+    display.textContent = Number(equationArray[0]) / Number(equationArray[1])
+  }
+  equationArray = []
+  operation = []
+  console.log(display, 'display')
+})
 
 const calculation = () => {
   const numberDisplay = buttonDisplayFunction(buttonsArray)
   const operatorOptions = operatorEvents(operatorArray)
   const numberButton = numberEvents(buttonsArray)
-  const equationresult = equationOptions(equationArray)
 
-  return numberDisplay, operatorOptions, numberButton, equationresult
+  return numberDisplay, operatorOptions, numberButton
 }
 
 calculation()
@@ -87,22 +93,3 @@ calculation()
 //   if(displaySequence[0] === equals(operator)){
 // return undefined
 //   }
-
-// const pressButtons = (buttonsArray) => {
-//   // debugger
-//   buttonsArray.forEach((button) => {
-//     // buttons.addEventListener('click', () => {
-//     console.log('button')
-//     display.textContent = button.value
-//     console.log(button.value)
-//     // })
-//   })
-// }
-// buttons.addEventListener('click', calculation)
-
-// const mapOperators = operatorArray.map(function (operator) {
-//   operator.addEventListener('click', () => {
-//     display.textContent = operator.value
-//     console.log(operator.value, 'maparray')
-//   })
-// })
