94de90f6f3cee6d27a75cf7b6ab015e5270d972b
Kurtis <kurtis@edge.app>
2023-04-05 17:44:47 -0700

    Add decimal conditions

--- calculator.js
+++ calculator.js
@@ -1,49 +1,64 @@
-// do 11 + 2 next
-
 const buttonEvents = document.querySelectorAll('[data-button]')
 const twobuttons = document.querySelectorAll('row-two')
 const buttons = document.getElementsByClassName('number-btn')
 const operators = document.getElementsByClassName('operator-btn')
-
-const display = document.getElementById('numberDisplay')
-let firstDigitArray = []
-let secondDigitArray = []
+const displayResult = document.getElementById('numberDisplay')
+const memory = document.getElementById('memoryDisplay')
 
 const buttonsArray = [...buttons]
 const operatorArray = [...operators]
-let first = ''
-let second = ''
-
+let firstDigitArray = []
+let secondDigitArray = []
 let operation = ''
+let memoryHolder = ''
+
+const clearButton = document.getElementById('clear')
+clearButton.addEventListener('click', () => {
+  displayResult.textContent = ''
+  firstDigitArray = []
+  secondDigitArray = []
+  operation = ''
+  memoryHolder = ''
+  addMemoryButton = ''
+})
 
 // display functions
 
 const buttonDisplayFunction = (buttonsArray) => {
   buttonsArray.forEach((button) => {
     button.addEventListener('click', () => {
-      let numberDisplay = (display.textContent = button.value)
-      numberDisplay
-      // if ((operation = [] && digitArray.length > 0)) {
-      // display.textContent = firstDigitArray.join('')
-      // }
+      displayResult.textContent = button.value
     })
   })
 }
 
 // perform equations
 
+// TODO:
+// 1. error:  When firstdigitarray contains a decimal, then a decimal can not be added to seconddigitarray
+// 2. need conditional when operator is pressed first, before a number
+// 3. after equal is pressed , need to be able to continue operations.  Clear the array
+
 const numberEvents = (buttonsArray) => {
   buttonsArray.forEach((button) => {
     button.addEventListener('click', () => {
+      if (firstDigitArray.includes('.') && button.value === '.') {
+        return
+      }
+
+      if (secondDigitArray.includes('.') && button.value === '.') {
+        return
+      }
+
       if (operation.length === 1) {
         secondDigitArray.push(button.value)
       } else {
         firstDigitArray.push(button.value)
       }
 
-      display.textContent = firstDigitArray.join('')
+      displayResult.textContent = firstDigitArray.join('')
       if (operation.length === 1 && firstDigitArray.length > 0) {
-        display.textContent = secondDigitArray.join('')
+        displayResult.textContent = secondDigitArray.join('')
       }
       console.log('firstDigitArray', firstDigitArray)
     })
@@ -53,46 +68,60 @@ const numberEvents = (buttonsArray) => {
 const operatorEvents = (operatorArray) => {
   operatorArray.forEach((operator) => {
     operator.addEventListener('click', () => {
-      // if numberArray is more than one index then join them together
-
-      display.textContent = ''
-      if (operation != []) operation = []
+      if (operation != '' && firstDigitArray === []) {
+        operation = ''
+      }
       operation = operator.value
       console.log('operation', operation)
-      // console.log(firstDigitArray.join(''), 'joined array')
     })
   })
 }
 
+// const operatorDisplayAction = (operatorArray) => {
+// operatorArray.map((operator) => {
+//   operator.addEventListener('click', () => {
+//     document.getElementById(display).style.display = 'none'
+
+//     document.getElementById(display).style.display = 'block'
+//   })
+// })
+// }
+
 const equalButton = document.getElementById('equal-btn')
 
 equalButton.addEventListener('click', () => {
-  if (firstDigitArray.length > 1) {
-    firstDigitArray.join('')
-  }
-  console.log('array', firstDigitArray, secondDigitArray)
-  let equation = []
+  // if (firstDigitArray.length > 1) {
+  //   firstDigitArray.join('')
+  // }
+  console.log('array', firstDigitArray, 'secondDigitArray', secondDigitArray)
 
   if (operation === '+') {
-    display.textContent =
+    displayResult.textContent =
       Number(firstDigitArray.join('')) + Number(secondDigitArray.join(''))
+
+    memoryHolder += displayResult.textContent
+    console.log('memoryHolder', memoryHolder)
   }
   if (operation === '-') {
-    display.textContent =
+    displayResult.textContent =
       Number(firstDigitArray.join('')) - Number(secondDigitArray.join(''))
+    memoryHolder += displayResult.textContent
+    console.log('memoryHolder', memoryHolder)
   }
   if (operation === '*') {
-    display.textContent =
+    displayResult.textContent =
       Number(firstDigitArray.join('')) * Number(secondDigitArray.join(''))
+    memoryHolder += displayResult.textContent
+    console.log('memoryHolder', memoryHolder)
   }
   if (operation === '/') {
-    display.textContent =
+    displayResult.textContent =
       Number(firstDigitArray.join('')) / Number(secondDigitArray.join(''))
+    memoryHolder += displayResult.textContent
+    console.log('memoryHolder', memoryHolder)
   }
-  firstDigitArray = []
-  secondDigitArray = []
-  operation = ''
-  console.log(display, 'display')
+  console.log('firstDigitArray', firstDigitArray)
+  firstDigitArray.length = 0
 })
 
 const calculation = () => {
@@ -105,16 +134,8 @@ const calculation = () => {
 
 calculation()
 
-// equals event listener
+let addMemoryButton = document.getElementById('addMemory')
 
-// const displaySequence = () => {
-// let displayString = ''
-// displayString =  Number(input)
-// }
-
-// // if(!(displaySequence.include(operator) ){
-//   return endefined
-// }
-//   if(displaySequence[0] === equals(operator)){
-// return undefined
-//   }
+addMemoryButton.addEventListener('click', () => {
+  memory.textContent = memoryHolder
+})
